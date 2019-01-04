c597ed37a5a46bd38657419c4ec353a76f0640b5
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-04 18:39:16 +0200

    .Changed: retreiving lap listing from API.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,62 @@
-import SocketIO from 'socket.io'
+import socketIO from 'socket.io'
+import http from 'http'
 import { roomLamps } from './database'
 
-// take from configurtion
-const ioSrv = new SocketIO(3000, {
-  pingInterval: 3600000 // testing only!
+// Todo: retreive configurtion here
+
+const port = 3000
+const pingInterval = 3600000 // testing only!
+
+const httpServer = http.createServer()
+
+const io = new socketIO(httpServer, {
+  pingInterval: pingInterval || undefined
 })
 
-ioSrv.on('connection', s => {
-  console.log(`a user connected: ${s.id}`)
+io.on('connection', s => {
+  console.log(`|--> [ ${s.id} ] : a user connected`)
+
+  s.emit('ok')
 
   s.on('disconnect', () => {
-    console.log(`a user disconnected: [ ${s.id} ]`)
+    console.log(`>--| [ ${s.id} ] : a user disconnected`)
   })
 
-  s.on('lamps-add', (lamp, room) => {
+  s.on('lamps-get_all', fn => {
+    console.log(`sending lamps array with ${roomLamps.count()} items.`)
+    function generateStates(lamps) {
+      function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1)) + min // The maximum is inclusive and the minimum is inclusive
+      }
+      let randomIds = []
+      while (randomIds.length < lamps.length * 0.33) {
+        randomIds.push(getRandomIntInclusive(1, lamps.length))
+      }
+      return lamps.map((l, i) => {
+        return {
+          ...l,
+          state: Number(!!randomIds.includes(++i))
+        }
+      })
+    }
+    fn(generateStates(roomLamps.data))
+  })
+
+  s.on('lamps-add', (lamp, room, fn) => {
     let lampObj = roomLamps.insertOne({
       name: `${lamp || '-lamp'} ${roomLamps.count() + 1}`,
-      room: `${room || '-tuba'}`,
-      creator: s.id
+      room: `${room || '-tuba'}`
     })
     console.log(`created: ${JSON.stringify(lampObj)}`)
-    s.emit(lampObj)
+    fn(lampObj)
   })
 })
+
+httpServer.listen(port, err => {
+  if (err) throw err
+  console.log(`==> listening on port *:${port}`)
+})
+
+console.log('planner-api init done')
