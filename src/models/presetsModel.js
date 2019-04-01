f4ced445b117c59c7810685ab3bcedc40185275c
Lauri Saar <Lauri.Saar@telia.ee>
2019-04-01 23:44:58 +0300

    #Added: added validation support to API Preset model: * added cron-parser NPM package as well.

--- src/models/presetsModel.js
+++ src/models/presetsModel.js
@@ -5,6 +5,7 @@ import {
   roomBlindsCollection
 } from '../persistence'
 import { transformItems } from './transforms'
+import cronParser from 'cron-parser'
 
 /**
  * Takes new object, saves to database, or returns `{error}`,
@@ -12,19 +13,33 @@ import { transformItems } from './transforms'
  * @param preset preset to insert to database.
  * @returns new preset's `{id}` or `{error}`.
  */
-export function add({ id, ...preset }) {
-  if (id && Number.parseInt(id) !== 0) {
-    return { error: `attempted object has {id} other than 0` }
-  }
+export function add(preset) {
   const doc = sanitize(preset)
+  const errors = validate(doc)
+  if (errors) {
+    return errors
+  }
   // ToDo error check
   return { id: presetsCollection.insertOne(doc).$loki }
 }
 
-export function update({ id, ...preset }) {
+export function update(preset) {
+  // ToDo implement validation
   const doc = sanitize(preset)
+  const errors = validate(doc)
+  if (errors) {
+    return errors
+  }
   // ToDo add error handling (Loki, sync vs async update!)
-  presetsCollection.update(Object.assign(presetsCollection.get(id), doc))
+  const dbDoc = presetsCollection.get(preset.id)
+  if (!dbDoc) {
+    return {
+      errors: [
+        `didn't found a document from database with {id}: "${preset.id}"`
+      ]
+    }
+  }
+  presetsCollection.update(Object.assign(dbDoc, doc))
   return { status: 'ok' }
 }
 
@@ -45,6 +60,69 @@ function sanitize({
     active,
     // setorder,
     devices: rawDevices.map(({ id, type, value }) => ({ id, type, value }))
+  }
+}
+
+function validate(doc) {
+  const errors = []
+  validateName(doc.name, errors)
+  validateSchedule(doc.schedule, errors)
+  validateActive(doc.active, errors)
+  validateDevices(doc.devices, errors)
+  return errors.length ? { errors } : null
+}
+
+const getErrorMessage = (propName, propValue) => {
+  return `attempted object didn't have valid {${propName}} value: "${propValue}"`
+}
+
+function validateName(name, errors) {
+  if (!name) {
+    errors.push(getErrorMessage('name', name))
+  }
+}
+
+function validateSchedule(schedule, errors) {
+  if (!schedule) {
+    errors.push(getErrorMessage('schedule', schedule))
+  } else {
+    const stringResult = cronParser.parseString(schedule)
+    if (Object.keys(stringResult.errors).length > 0) {
+      errors.push(
+        getErrorMessage('schedule', JSON.stringify(stringResult.errors))
+      )
+    }
+  }
+}
+
+function validateActive(active, errors) {
+  if (
+    !(
+      active === false ||
+      active === true ||
+      active === 'false' ||
+      active === 'true'
+    )
+  ) {
+    errors.push(getErrorMessage('active', active))
+  }
+}
+
+function validateDevices(devices, errors) {
+  if (devices && devices.length > 0) {
+    devices.forEach(({ id, type, value }) => {
+      if (!id) {
+        errors.push(getErrorMessage('devices.id', id))
+      }
+      if (!type) {
+        errors.push(getErrorMessage('devices.type', type))
+      }
+      if (value < 0 && value > 1) {
+        errors.push(getErrorMessage('devices.value', value))
+      }
+    })
+  } else {
+    errors.push(getErrorMessage('devices', JSON.stringify(devices)))
   }
 }
 
