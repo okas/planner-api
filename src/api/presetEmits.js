235dff9d4aadd6604d0f7dde9c67b1d513153bca
Lauri Saar <Lauri.Saar@telia.ee>
2019-04-16 01:33:00 +0300

    #Changed: added log message to presetEmits module.

--- src/api/presetEmits.js
+++ src/api/presetEmits.js
@@ -1,13 +1,21 @@
 import messageBus, { PRESET__UPDATED_DEVICE_DELETED } from '../messageBus'
 
+const room = 'preset'
+
 /**
  *  SocketIO broadcaster. Uses `{messageBus}` module to listen events from app.
  *  @param {SocketIO.Server} io
  */
 export default function(io) {
-  const ioRoom = io.to('preset')
+  const ioRoom = io.to(room)
+
+  function getLogPrefix() {
+    return `[ API: presetEmitter ], room "${room}" : `
+  }
 
   messageBus.on(PRESET__UPDATED_DEVICE_DELETED, preset => {
     ioRoom.emit('preset__api_update', preset)
+    // ToDo receive and pass amy error infor of model/database?
+    console.log(`${getLogPrefix()}Sent updated presets's status, no errors.`)
   })
 }
