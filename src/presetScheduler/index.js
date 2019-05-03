7f21080917b1e519ac71d6356c341c48494d4192
Lauri Saar <Lauri.Saar@telia.ee>
2019-05-03 17:48:34 +0300

    #Changed: moved presetSheduler to folder module, renamed file to index.js

--- src/presetScheduler/index.js
+++ src/presetScheduler/index.js
@@ -1,19 +1,13 @@
 import nodeShedule from 'node-schedule'
-import { presetCollection } from './persistence'
 
 export default function initScheduler() {
-  runPresetJobs()
   presetCollection.addListener('delete', collDeletedHandler)
   presetCollection.addListener('insert', collInsertHandler)
   presetCollection.addListener('update', collUpdateHandler)
 }
 
-function runPresetJobs() {
   presetCollection
     .chain('findRunnablePresets')
-    .data()
-    .forEach(initiateJob)
-  logJobCount()
 }
 
 function collDeletedHandler({ id }) {
@@ -26,7 +20,6 @@ function collDeletedHandler({ id }) {
 
 function collInsertHandler(docPreset) {
   if (docPreset.active && docPreset.devices.length > 0) {
-    initiateJob(docPreset)
     logJobCount()
   }
 }
@@ -36,17 +29,8 @@ function collUpdateHandler(docPreset) {
   collInsertHandler(docPreset)
 }
 
-function initiateJob({ id, schedule, devices }) {
   nodeShedule.scheduleJob(id.toString(), schedule, fireDate => {
-    presetTask(devices, fireDate)
   })
-}
-
-function presetTask(devices, fireDate = new Date()) {
-  // ToDo some async way?
-  devices.forEach(d =>
-    console.log(`at "${fireDate}"`, 'run preset device [hardware] of:', d)
-  )
 }
 
 function logJobCount() {
