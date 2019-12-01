4857653d616ac335f350fd6f6233373bec93fafa
Lauri Saar <lauri.saar@outlook.com>
2019-12-01 20:44:54 +0200

    Add: IotNode Strategy generic Present&Lost handler
    * using `action` property;
    * Lost reuses Common Lost action.

--- src/mqtt-client/strategies/iotnode.js
+++ src/mqtt-client/strategies/iotnode.js
@@ -1,9 +1,16 @@
+import messageBus, {
+  MQTT__BLIND_LOST,
+  MQTT__BLIND_PRESENT,
+  MQTT__LAMP_LOST,
+  MQTT__LAMP_PRESENT
+} from '../../messageBus'
 import { ExistingDocumentError, ValidationErrors } from '../../model/errors'
 import * as model from '../../model/iotnodeModel'
 import {
   getDeviceCommonTopicsWithOthers,
   getTopicBaseDevice
 } from '../utilities'
+import { getActionDevicePresentLost } from './commonSyncActions'
 
 const type = 'iotnode'
 const baseTopic = getTopicBaseDevice(type)
@@ -93,6 +100,51 @@ function getTaskResult(id, command, rawPayload) {
   }
 }
 
+const mbEventsOutput = new Map([
+  ['lamp_present', MQTT__LAMP_PRESENT],
+  ['lamp_lost', MQTT__LAMP_LOST],
+  ['blind_present', MQTT__BLIND_PRESENT],
+  ['blind_lost', MQTT__BLIND_LOST]
+])
+
+/**
+ * @type {import('../typedefs').MQTTAction}
+ */
+function devicePresentHandler(iotId, mqttPayload) {
+  const { outputs: plOuts } =
+    mqttPayload && JSON.parse(mqttPayload.toString() || null)
+  const { outputs: dbOuts } = model.getById(iotId.toString())
+  dbOuts
+    .filter(o => o.usage)
+    .forEach(({ id: dbOId, usage }) => {
+      const mbEvent = mbEventsOutput.get(`${usage}_present`)
+      if (mbEvent) {
+        return
+      }
+      const { id: _id, usage: _usage, ...rest } = plOuts.find(
+        // eslint-disable-next-line eqeqeq
+        ({ id }) => id == dbOId
+      )
+      messageBus.emit(mbEvent, { id: dbOId, ...rest })
+    })
+}
+
+/**
+ * @type {import('../typedefs').MQTTAction}
+ */
+function deviceLostHandler(iotId, mqttPayload) {
+  const { outputs: dbOuts } = model.getById(iotId.toString())
+  dbOuts
+    .filter(o => o.usage)
+    .forEach(({ id, usage }) => {
+      const mbEvent = mbEventsOutput.get(`${usage}_lost`)
+      if (!mbEvent) {
+        return
+      }
+      getActionDevicePresentLost(mbEvent)(id, mqttPayload)
+    })
+}
+
 /**
  * Public API
  * @type {import('../typedefs').MQTTStrategy}
@@ -106,5 +158,9 @@ export default {
   asyncTasks: new Map([
     [cmndInit, mqttInitHandler],
     [cmndInitUpdate, mqttInitUpdateHandler]
+  ]),
+  actions: new Map([
+    ['present', devicePresentHandler],
+    ['lost', deviceLostHandler]
   ])
 }
