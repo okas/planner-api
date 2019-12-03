bc481ae9774f6dccb7fad6b943c9c0569098c72e
Lauri Saar <lauri.saar@outlook.com>
2019-12-04 01:20:25 +0200

    Refactor: added JSDoc for binary data

--- src/mqtt-client/typedefs.js
+++ src/mqtt-client/typedefs.js
@@ -8,28 +8,47 @@ export const unused = {}
  */
 
 /**
+ * Topic and payload for ready for MQTT publish.
+ * @typedef {object} MQTTTaskResultBinary
+ * @property {string} topic
+ * @property {Buffer} [payload]
+ */
+
+/**
  * @callback MQTTAsyncTask
  * @param {number | string} id
- * @param {Buffer | string} mqttPayload
+ * @param {Buffer} mqttPayload
  * @returns {Promise.<MQTTTaskResult>}
+ */
+
+/**
+ * @callback MQTTAsyncTaskBinary
+ * @param {number | string} id
+ * @param {Buffer} mqttPayload
+ * @returns {Promise.<MQTTTaskResultBinary>}
  */
 
 /**
  * @callback MQTTAction
  * @param {number | string} id
- * @param {Buffer | string} [mqttPayload]
+ * @param {Buffer} [mqttPayload]
  * @returns {void}
  */
 
 /**
  * @callback MQTTResponseParser
- * @param {Buffer | string} responsePayload
- * @returns {object}
+ * @param {Buffer} responsePayload
+ * @returns {any}
  */
 
 /**
- * Topic and payload for ready for MQTT publish.
+ * Topic and payload ready for MQTT publish.
  * @typedef {MQTTTaskResult & {responseParser?: MQTTResponseParser}} MQTTPublishCommandResult
+ */
+
+/**
+ * Topic and payload ready for MQTT publish.
+ * @typedef {MQTTTaskResultBinary & {responseParser?: MQTTResponseParser}} MQTTPublishCommandResultBinary
  */
 
 /**
@@ -40,11 +59,18 @@ export const unused = {}
  */
 
 /**
+ * @callback MQTTPublishCommandBinary
+ * @param {object} data
+ * @param {string} [sender]
+ * @returns {MQTTPublishCommandResultBinary}
+ */
+
+/**
  * @typedef {object} MQTTStrategy
  * @property {string} type
  * @property {string | string[] | import('mqtt').ISubscriptionMap[]} subscriptions
- * @property {Map.<string, MQTTAsyncTask>} [asyncTasks]
- * @property {Map.<symbol, Function>} [publishCommands]
+ * @property {Map.<string, MQTTAsyncTask | MQTTAsyncTaskBinary>} [asyncTasks]
+ * @property {Map.<symbol, MQTTPublishCommand | MQTTPublishCommandBinary>} [publishCommands]
  * @property {Map.<string, MQTTAction>} [actions]
  */
 
